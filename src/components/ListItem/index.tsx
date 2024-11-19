6f105b92f44140d615d97b7004abad576944ce8c
orangejack <79419803+kingcat47@users.noreply.github.com>
2024-11-19 12:39:10 +0900

    ListItem component props로 안해놔서 수정

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,17 +1,24 @@
 import SvgIcon from "../SvgIcon";
 import styles from "./styles.module.scss";
 import ArrowIcon from "../../assets/icon/arrow_forward.svg?react";
-export default function ListItem() {
+
+interface ListItemProps {
+  whatclass?: string;
+  title?: string;
+  many?: string;
+}
+
+export default function ListItem({ whatclass, title, many }: ListItemProps) {
   return (
     <div className={styles.supperjjangjjangman}>
       <div className={styles.container}>
         <div className={styles.main}>
           <div className={styles.right}>
-            <div className={styles.title}>SaveQuest</div>
-            <div className={styles.text}>1학년 4반</div>
+            <div className={styles.title}>SaveQuest{title}</div>
+            <div className={styles.text}>1학년 4반{whatclass}</div>
           </div>
           <div className={styles.left}>
-            <div className={styles.text}>(4)</div>
+            <div className={styles.text}>(4{many})</div>
             <SvgIcon
               width={16}
               height={16}
